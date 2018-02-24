f5d4f418517985981d94a3b11bd56cc07574360a
khalil <kkhalaf@ltu.edu>
2018-02-23 22:22:23 -0500

    Added Binding Example

--- MvvmTemplate/ViewModels/MainViewModel.cs
+++ MvvmTemplate/ViewModels/MainViewModel.cs
@@ -7,8 +7,44 @@ using System.Threading.Tasks;
 
 namespace MvvmTemplate.ViewModels
 {
-    class MainViewModel : Screen
+    public class MainViewModel : Screen
     {
+        private string _firstName = "First";
+        private string _lastName = "Last";
 
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName); // this notifies the change to whoever binded to FirstName property
+                NotifyOfPropertyChange(() => FullName); // this notifies the change to whoever binded to FullName property
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName); // this notifies the change to whoever binded to LastName property
+                NotifyOfPropertyChange(() => FullName); // this notifies the change to whoever binded to FullName property
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return $" { FirstName } { LastName } ";
+            }
+
+        }
     }
 }
